ae34cc2865166987f495778ed69d9f47dadce8d1
Adron Hall <adronhall@gmail.com>
2021-02-23 13:02:34 -0800

    Attempting first deploy of Hasura v.2.0.

--- root/main.tf
+++ root/main.tf
@@ -50,7 +50,7 @@ resource "azurerm_container_group" "hasura" {
 
   container {
     name   = "hasura-data-layer"
-    image  = "hasura/graphql-engine:v1.3.3"
+    image  = "hasura/graphql-engine:v2.0.0-alpha.1"
     cpu    = "0.5"
     memory = "1.5"
 
